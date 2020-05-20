16a62497d0d18719e2cd42da85b1e1a0e6ecf915
Ben Lee <benjamintlee600@gmail.com>
2020-05-20 10:55:13 +0100

    altered styling of project card and added animation

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,17 +4,26 @@ import '../styles/projectCard.css';
 // import Button from 'react-bootstrap/Button';
 // import CardGroup from 'react-bootstrap/CardDeck';
 import pokemonLogo from '../assets/pokemon_logo.png';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const ProjectCard = () => {
   return (
-    <section className="project-card-container">
-      <div className="project-title-container">
-        <h4 className="project-card-title">Project 1</h4>
-      </div>
+    <motion.div
+      animate
+      className="project-card-container"
+      whileHover={{ scale: 1.05 }}
+      // whileTap={{ scale: 0.9 }}
+    >
+      {/* <div className="project-title-container">
+        <h4 className="project-card-title" animate>
+          Project 1
+        </h4>
+      </div> */}
       <div className="project-image-container">
         <img src={pokemonLogo} alt="pokemon-logo" className="project-img" />
       </div>
       <div className="project-info-container">
+        <h4 className="project-card-title">Project 1</h4>
         <p className="project-info-text">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia ad
           adipisci magnam consequatur animi aut nam amet fuga, nulla nisi?
@@ -22,7 +31,7 @@ const ProjectCard = () => {
           pariatur.
         </p>
       </div>
-    </section>
+    </motion.div>
   );
 };
 export default ProjectCard;
