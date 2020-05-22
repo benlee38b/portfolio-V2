a90b379cff07e9da64aed85928bbde0abace9a22
Ben Lee <benjamintlee600@gmail.com>
2020-05-22 15:50:18 +0100

    added animation for contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as emailjs from 'emailjs-com';
 import '../styles/contact.css';
-
+import FadeIn from 'react-fade-in';
 import Alert from 'react-bootstrap/Alert';
 import Button from '@material-ui/core/Button';
 import { Form, FormGroup, Label, Input } from 'reactstrap';
@@ -80,59 +80,61 @@ class ContactForm extends Component {
         </Alert>
 
         <main className="contact-form-container">
-          <h1 className="p-heading1">Get in Touch</h1>
-          <Form onSubmit={this.handleSubmit.bind(this)}>
-            <FormGroup controlId="formBasicEmail">
-              <Label className="text-muted">Email address</Label>
-              <Input
-                type="email"
-                name="email"
-                value={this.state.email}
-                className="text-primary"
-                onChange={this.handleChange.bind(this, 'email')}
-                placeholder="Enter email"
-                required
-              />
-            </FormGroup>
-            <FormGroup controlId="formBasicName">
-              <Label className="text-muted">Name</Label>
-              <Input
-                type="text"
-                name="name"
-                value={this.state.name}
-                className="text-primary"
-                onChange={this.handleChange.bind(this, 'name')}
-                placeholder="Name"
-                required
-              />
-            </FormGroup>
-            <FormGroup controlId="formBasicSubject">
-              <Label className="text-muted">Subject</Label>
-              <Input
-                type="text"
-                name="subject"
-                className="text-primary"
-                value={this.state.subject}
-                onChange={this.handleChange.bind(this, 'subject')}
-                placeholder="Subject"
-                required
-              />
-            </FormGroup>
-            <FormGroup controlId="formBasicMessage">
-              <Label className="text-muted">Message</Label>
-              <Input
-                type="textarea"
-                name="message"
-                className="text-primary"
-                value={this.state.message}
-                onChange={this.handleChange.bind(this, 'message')}
-                required
-              />
-            </FormGroup>
-            <Button variant="contained" color="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+          <FadeIn delay="75" transitionDuration="1500">
+            <h1 className="p-heading1">Get in Touch</h1>
+            <Form onSubmit={this.handleSubmit.bind(this)}>
+              <FormGroup controlId="formBasicEmail">
+                <Label className="text-muted">Email address</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  value={this.state.email}
+                  className="text-primary"
+                  onChange={this.handleChange.bind(this, 'email')}
+                  placeholder="Enter email"
+                  required
+                />
+              </FormGroup>
+              <FormGroup controlId="formBasicName">
+                <Label className="text-muted">Name</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  className="text-primary"
+                  onChange={this.handleChange.bind(this, 'name')}
+                  placeholder="Name"
+                  required
+                />
+              </FormGroup>
+              <FormGroup controlId="formBasicSubject">
+                <Label className="text-muted">Subject</Label>
+                <Input
+                  type="text"
+                  name="subject"
+                  className="text-primary"
+                  value={this.state.subject}
+                  onChange={this.handleChange.bind(this, 'subject')}
+                  placeholder="Subject"
+                  required
+                />
+              </FormGroup>
+              <FormGroup controlId="formBasicMessage">
+                <Label className="text-muted">Message</Label>
+                <Input
+                  type="textarea"
+                  name="message"
+                  className="text-primary"
+                  value={this.state.message}
+                  onChange={this.handleChange.bind(this, 'message')}
+                  required
+                />
+              </FormGroup>
+              <Button variant="contained" color="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </FadeIn>
         </main>
       </>
     );
